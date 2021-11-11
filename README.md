3f11bdc39aaa06e36af0018840e7df677f6c3cbf
Darryn Codes <hello@darryncodes.co.uk>
2021-11-11 19:45:39 +0000

    adding README.md

---
+++ README.md
@@ -0,0 +1,49 @@
+# Frontend Mentor - Huddle landing page with curved sections solution
+
+This is a solution to the [Huddle landing page with curved sections challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/huddle-landing-page-with-curved-sections-5ca5ecd01e82137ec91a50f2). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+
+### Screenshot
+
+![](./desktop-screenshot.png)
+![](./mobile-screenshot.png)
+
+### Links
+
+- [GitHub repo](https://github.com/darryncodes/huddle-landing-page-with-curved-sections)
+- [Solution URL](https://darryncodes.github.io/huddle-landing-page-with-curved-sections/)
+- [Live Site URL](https://www.darryncodes.co.uk/pages/huddle.html)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- SCSS
+- JS
+- Flexbox
+- Mobile-first workflow
+- BEM methodology
+
+## Author
+
+- Work in progress portfolio site - [@darryncodes](https://www.darryncodes.co.uk/)
+- Frontend Mentor - [@darryncodes](https://www.frontendmentor.io/profile/darryncodes)
--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,48 @@
-"use strict";
+'use strict';
 
-const form = document.getElementById("form");
-const email = document.getElementById("email");
-const group = document.querySelector(".form__group");
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const group = document.querySelector('.form__group');
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
+form.addEventListener('submit', e => {
+  e.preventDefault();
 
-    checkInputs();
+  checkInputs();
 
-    email.value = "";
-    setTimeout(function () {
-        group.classList.remove("success");
-    }, 1000);
+  email.value = '';
+  setTimeout(function () {
+    group.classList.remove('success');
+  }, 1000);
 });
 
 function checkInputs() {
-    const emailValue = email.value.trim();
+  const emailValue = email.value.trim();
 
-    if (emailValue === "") {
-        setErrorFor(email, "Email cannot be empty");
-    } else if (!isEmail(emailValue)) {
-        setErrorFor(email, "Check your email please");
-    } else {
-        setSuccessFor(email);
-    }
+  if (emailValue === '') {
+    setErrorFor(email, 'Email cannot be empty');
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email, 'Check your email please');
+  } else {
+    setSuccessFor(email);
+  }
 }
 
 function setErrorFor(input, message) {
-    const formGroup = input.parentElement;
-    const small = formGroup.querySelector("small");
+  const formGroup = input.parentElement;
+  const small = formGroup.querySelector('small');
 
-    small.innerText = message;
+  small.innerText = message;
 
-    formGroup.className = "form__group error";
+  formGroup.className = 'form__group error';
 }
 
 function setSuccessFor(input) {
-    const formGroup = input.parentElement;
-    formGroup.className = "form__group success";
+  const formGroup = input.parentElement;
+  formGroup.className = 'form__group success';
 }
 
 function isEmail(email) {
-    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-        email
-    );
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
 }
